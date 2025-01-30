9bae0c96c8ec7010e6b42bb2c6b5dad6772dfc85
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-30 09:46:12 +0530

    Update

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const serverless = require("serverless-http"); // Required for Vercel
 
 app.get("/", (req, res) => {
   res.send("Hello, Vercel!");
 });
 
-module.exports = app; 
+module.exports = app;
+module.exports.handler = serverless(app);
