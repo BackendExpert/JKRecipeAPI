4a1fa1b674d81d51333976976054c8c7a4703875
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-02-03 07:43:14 +0530

    Update

--- controller/APIController.js
+++ controller/APIController.js
@@ -21,7 +21,20 @@ const APIController = {
 
     onedata: async(req, res) => {
         try{
-            const recipeID = req.params.id
+            // const recipeID = req.params.id
+            const recipeID = Number(req.params.id);
+
+
+            const sourceFile = path.join(__dirname, "../DataJson/api.json");
+            fs.readFile(sourceFile, 'utf8', (err, data) => {
+                if(err){
+                    return res.json({ Error: "no Data found..."})
+                }
+                const jsondata = JSON.parse(data);
+                const recipe = jsondata.find(item => item.id === recipeID);
+
+                return res.json({ Status: "Success", Result: recipe })
+            })
         }
         catch(err){
             console.log(err)
